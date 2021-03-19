c1e67c7c74ba73c1ba357678d6a8762ceae4e82d
Geovane Silva <geoscosta@gmail.com>
2021-03-18 23:08:58 -0300

    Implementando o Entity

---
+++ src/Api.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+namespace Api.Domain.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        private DateTime? _createAt;
+        public DateTime? CreatAt
+        {
+            get { return _createAt; }
+            set { _createAt = (value == null ? DateTime.UtcNow : value); }
+        }
+
+        public DateTime? UpdateAt { get; set; }
+        
+    }
+}
